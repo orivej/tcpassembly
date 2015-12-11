d1f04496c306cbaaff4659f821b383052adf0404
Orivej Desh <orivej@gmx.fr>
2015-12-11 02:49:52 +0000

    Add bidistream and MySQL splitter.

---
+++ bidistream/bidistream.go
@@ -0,0 +1,88 @@
+// Package bidistream provides bidirectional TCP stream reassembly.
+//
+// Bidirection TCP reassembly in general is protocol specific.  This reassembler
+// works for protocols those endpoints at any moment are either readers or
+// writers, and change their role only in response to writes from the other
+// endpoint.
+//
+// For each flow, a user-supplied BidiStreamFactory will create a
+// user-implemented BidiStream.
+package bidistream
+
+import (
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/tcpassembly"
+)
+
+type BidiStream interface {
+	// Reassembled is called with an endpoint number (0 or 1) and packets in
+	// the reassembled order.
+	Reassembled(uint8, []tcpassembly.Reassembly)
+	ReassemblyComplete()
+}
+
+type BidiStreamFactory interface {
+	New(netFlow, tcpFlow gopacket.Flow) BidiStream
+}
+
+type key struct {
+	net, transport gopacket.Flow
+}
+
+type myStream struct {
+	index uint8
+	bytes []byte
+	bidi  *bidi
+	done  bool
+}
+
+type bidi struct {
+	a, b *myStream
+	s    BidiStream
+}
+
+type bidiFactory struct {
+	bidiMap     map[key]*bidi
+	userFactory BidiStreamFactory
+}
+
+func NewStreamFactory(userFactory BidiStreamFactory) (tcpassembly.StreamFactory, func()) {
+	f := &bidiFactory{map[key]*bidi{}, userFactory}
+	return f, f.Complete
+}
+
+func (f *bidiFactory) New(netFlow, tcpFlow gopacket.Flow) tcpassembly.Stream {
+	s := &myStream{}
+	k := key{netFlow, tcpFlow}
+	bd := f.bidiMap[k]
+	if bd == nil {
+		bd = &bidi{a: s, s: f.userFactory.New(netFlow, tcpFlow)}
+		f.bidiMap[key{netFlow.Reverse(), tcpFlow.Reverse()}] = bd
+	} else {
+		s.index = 1
+		bd.b = s
+		delete(f.bidiMap, k)
+	}
+	s.bidi = bd
+	return s
+
+}
+
+func (f *bidiFactory) Complete() {
+	for k := range f.bidiMap {
+		f.bidiMap[k].s.ReassemblyComplete()
+		delete(f.bidiMap, k)
+	}
+}
+
+func (s *myStream) Reassembled(rs []tcpassembly.Reassembly) {
+	s.bidi.s.Reassembled(s.index, rs)
+}
+
+func (s *myStream) ReassemblyComplete() {
+	s.done = true
+	bd := s.bidi
+	if bd.b != nil && bd.a.done && bd.b.done {
+		bd.s.ReassemblyComplete()
+	}
+}
---
+++ mysqlsplit/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"orivej/tcpassembly/bidistream"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcapgo"
+	"github.com/google/gopacket/tcpassembly"
+	"github.com/orivej/e"
+)
+
+var nextID int
+
+func NextID() int {
+	nextID++
+	return nextID
+}
+
+// splitStream implements bidistream.BidiStream
+type splitStream struct {
+	clientIndex uint8
+	flowID      int
+	lastIndex   uint8
+	dirname     string
+	data        []byte
+	nextSize    int
+	firstSeen   time.Time
+}
+
+func (s *splitStream) ReassemblyComplete() {}
+
+func (s *splitStream) Reassembled(index uint8, rs []tcpassembly.Reassembly) {
+	if index != s.clientIndex {
+		return
+	}
+	for _, r := range rs {
+		if s.firstSeen.IsZero() {
+			s.firstSeen = r.Seen
+		}
+
+		s.data = append(s.data, r.Bytes...)
+
+		for {
+			if s.nextSize == 0 && len(s.data) >= 4 {
+				s.nextSize = int(4 + 0xFFFFFF&binary.LittleEndian.Uint32(s.data))
+			}
+
+			if s.nextSize == 0 || len(s.data) < s.nextSize {
+				break
+			}
+
+			if s.data[4] == 3 { // Command: Query
+				if len(s.dirname) == 0 {
+					s.dirname = fmt.Sprintf("%08d-%v", s.flowID, s.firstSeen.UnixNano()/1000)
+					err := os.Mkdir(s.dirname, 0777)
+					e.Exit(err)
+				}
+
+				out, err := os.Create(fmt.Sprintf("%v/%v", s.dirname, r.Seen.UnixNano()/1000))
+				e.Exit(err)
+				defer e.CloseOrExit(out)
+				_, err = out.Write(s.data[5:s.nextSize])
+				e.Exit(err)
+			}
+
+			s.data = s.data[s.nextSize:]
+			s.nextSize = 0
+		}
+	}
+}
+
+// splitStreamFactory implements bidistream.BidiStreamFactory for splitStream
+type splitStreamFactory struct{}
+
+var mysqlServerEndpoint = layers.NewTCPPortEndpoint(layers.TCPPort(3306))
+
+func (f *splitStreamFactory) New(netFlow, tcpFlow gopacket.Flow) bidistream.BidiStream {
+	s := &splitStream{flowID: NextID()}
+	if tcpFlow.Src() == mysqlServerEndpoint {
+		// Client stream is the other stream (stream 1) of the connection.
+		s.clientIndex = 1
+	}
+	return s
+}
+
+func main() {
+	flag.Parse()
+
+	in, err := pcapgo.NewReader(os.Stdin)
+	e.Exit(err)
+
+	var (
+		eth     layers.Ethernet
+		ip4     layers.IPv4
+		ip6     layers.IPv6
+		tcp     layers.TCP
+		payload gopacket.Payload
+	)
+
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp, &payload)
+	factory, factoryComplete := bidistream.NewStreamFactory(&splitStreamFactory{})
+	streamPool := tcpassembly.NewStreamPool(factory)
+	assembler := tcpassembly.NewAssembler(streamPool)
+
+	decodedLayers := make([]gopacket.LayerType, 0, 10)
+
+	for {
+		data, ci, err := in.ReadPacketData()
+		if err == io.EOF {
+			assembler.FlushAll()
+			factoryComplete()
+			return
+		}
+		e.Exit(err)
+
+		err = parser.DecodeLayers(data, &decodedLayers)
+		e.Exit(err)
+
+		for _, typ := range decodedLayers {
+			switch typ {
+			case layers.LayerTypeTCP:
+				flow := tcp.TransportFlow()
+				assembler.AssembleWithTimestamp(flow, &tcp, ci.Timestamp)
+			}
+		}
+	}
+}
